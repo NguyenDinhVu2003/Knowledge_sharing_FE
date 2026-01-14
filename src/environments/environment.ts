2cbfcdddcb1ee7336161c38dc8312fdb4f3ca2a3
nguyendinhvu20215257@gmail.com <nguyendinhvu20215257@gmail.com>
2026-01-14 22:31:11 +0700

    change api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api.com/api', // Update with production URL
+  apiUrl: 'https://knowledgesharingbe-production.up.railway.app/api', // Update with production URL
   tokenKey: 'auth_token',
   wsEnabled: true  // Enable/disable WebSocket
 };
