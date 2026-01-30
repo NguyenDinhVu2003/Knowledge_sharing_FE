bc62e0dd7fe982d39580438fdbb6cf724c787490
nguyendinhvu20215257@gmail.com <nguyendinhvu20215257@gmail.com>
2026-01-30 11:49:12 +0700

    railway

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -10,6 +10,10 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Prerender
   },
   {
+    path: 'documents/:id',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Client
   }
